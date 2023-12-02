4b34651104caa1b2ee6842c0430803191c3e0415
sadishaaaa <sthasadisha2019@gmail.com>
2023-12-02 15:01:44 +0545

    player control when keys are pressed

--- JAVASCRIPT/JsAssignment-4/Script/index.js
+++ JAVASCRIPT/JsAssignment-4/Script/index.js
@@ -23,6 +23,7 @@ class Player {
   }
   update() {
     this.draw();
+    this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
     if (this.position.y + this.height + this.velocity.y < canvas.height)
       this.velocity.y += GRAVITY;
@@ -37,6 +38,14 @@ const player2 = new Player({
   x: 300,
   y: 100,
 });
+const keys = {
+  d: {
+    pressed: false,
+  },
+  a: {
+    pressed: false,
+  },
+};
 function animate() {
   window.requestAnimationFrame(animate);
   //giving color to rectangle
@@ -45,7 +54,33 @@ function animate() {
   c.fillRect(0, 0, canvas.width, canvas.height);
   player.update();
   player2.update();
+  player.velocity.x = 0;
+  if (keys.d.pressed) player.velocity.x = 5;
+  else if (keys.a.pressed) player.velocity.x = -5;
 }
 
 //activating anumation loop
 animate();
+window.addEventListener("keydown", (event) => {
+  switch (event.key) {
+    case "d":
+      keys.d.pressed = true;
+      break;
+    case "a":
+      keys.a.pressed = true;
+      break;
+    case "w":
+      player.velocity.y = -10;
+      break;
+  }
+});
+window.addEventListener("keyup", (event) => {
+  switch (event.key) {
+    case "d":
+      keys.d.pressed = false;
+      break;
+    case "a":
+      keys.a.pressed = false;
+      break;
+  }
+});
