cc829c062f7c2d6d870aebbd8a3f68eae8af88f5
sadishaaaa <sthasadisha2019@gmail.com>
2023-12-25 22:27:41 +0545

    design done with all the functionalir=ties

--- TYPESCRIPT/to-do-list/src/main.ts
+++ TYPESCRIPT/to-do-list/src/main.ts
@@ -44,8 +44,7 @@ function removeTask(id: string): void {
   taskList.removeTaskById(id);
   render();
 }
-
-function renderList(tasks: TaskList) {
+function renderList(tasks: TaskList, status: string) {
   if (!taskListElement) throw new Error("DOM element not found");
 
   taskListElement.innerHTML = "";
@@ -70,36 +69,118 @@ function renderList(tasks: TaskList) {
     taskValue.classList.add("task-item-value");
     taskValue.textContent = task.value;
 
-    const status = document.createElement("div");
-    status.classList.add("task-status");
-    status.textContent = task.completed ? "Completed" : "Assigned";
+    const statusElement = document.createElement("div");
+    statusElement.classList.add("task-status");
+    statusElement.classList.add(task.completed ? "completed" : "assigned");
+    statusElement.textContent = task.completed ? "Completed" : "Assigned";
 
     const assignedDate = document.createElement("div");
     assignedDate.classList.add("task-date");
+    assignedDate.textContent = task.getAssignedDate()
+      ? ` ${task.getAssignedDate()!.toLocaleString()}`
+      : "";
 
-    // Inside the renderList function
     const removeButton = document.createElement("button");
     removeButton.textContent = "Remove";
     removeButton.addEventListener("click", () => {
       removeTask(task.id);
     });
-
-    label.appendChild(removeButton);
-    // Check if assignedDate is not null before calling toLocaleString
-    assignedDate.textContent = task.getAssignedDate()
-      ? ` ${task.getAssignedDate()!.toLocaleString()}`
-      : "";
-
-    label.appendChild(inputField);
     label.appendChild(taskValue);
-    label.appendChild(status);
     label.appendChild(assignedDate);
+    label.appendChild(inputField);
+    label.appendChild(statusElement);
     label.appendChild(removeButton);
+
     element.appendChild(label);
 
     taskListElement.appendChild(element);
   });
+  const clearAllButton = document.createElement("button");
+  clearAllButton.textContent = "Clear All";
+  clearAllButton.style.background = "red";
+  clearAllButton.style.color = "#fff";
+  clearAllButton.style.borderRadius = "4px";
+  clearAllButton.style.borderRadius = "4px";
+  clearAllButton.style.border = "none";
+  clearAllButton.style.padding = "0.6rem";
+
+  clearAllButton.addEventListener("click", () => {
+    clearAllTasks(status);
+  });
+
+  taskListElement.appendChild(clearAllButton);
+ 
+
+
+  function showReminderWithDelay() {
+    setTimeout(() => {
+      if (hasRemainingTasks()) {
+        showReminder();
+      }
+    }, 3000); 
+  }
+
+  showReminderWithDelay();
 }
+function clearAllTasks(status: string) {
+  if (status === "completed") {
+    taskList.list = taskList.list.filter((task) => !task.completed);
+  } else if (status === "remaining") {
+    taskList.list = taskList.list.filter((task) => task.completed);
+  } else {
+    taskList.list = [];
+  }
+
+  render();
+}
+function showReminder() {
+  alert("This is reminder to complete your assigned task!");
+}
+function hasRemainingTasks(): boolean {
+  return taskList.list.some((task) => !task.completed);
+}
+
+document.getElementById("remaining-tasks")?.addEventListener("click", () => {
+  render("", "remaining");
+});
+
+document.getElementById("completed-tasks")?.addEventListener("click", () => {
+  render("", "completed");
+});
+const tabs = document.querySelectorAll(".tab");
+
+function tabClickHandler(this: HTMLElement) {
+  // Remove the 'active' class from all tabs
+  tabs.forEach((t: HTMLElement) => t.classList.remove("active"));
+
+  // Add the 'active' class to the clicked tab
+  this.classList.add("active");
+
+  // Call the corresponding render function based on the clicked tab
+  const tabId = this.id;
+  switch (tabId) {
+  case "remaining-tasks":
+    render("", "remaining");
+    break;
+  case "completed-tasks":
+    render("", "completed");
+    break;
+  case "all-tasks":
+    render("", "");
+    break;
+  default:
+    break;
+  }
+}
+
+// Convert NodeList to array using Array.from
+Array.from(tabs).forEach((tab: HTMLElement) => {
+  tab.addEventListener("click", tabClickHandler);
+});
+
+tabs.forEach((tab: HTMLElement) => {
+  tab.addEventListener("click", tabClickHandler);
+});
 
 searchInput?.addEventListener("input", (e) => {
   const searchParam = (e.target as HTMLInputElement)?.value;
@@ -115,10 +196,16 @@ addTaskButton?.addEventListener("click", () => {
   }
 });
 
-function render(searchParam: string = "") {
+function render(searchParam: string = "", status: string = "") {
   const filteredTaskList = search(taskList, searchParam);
+  const filteredByStatus = status
+    ? new TaskList(
+      filteredTaskList.list.filter(
+        (task) => task.completed === (status === "completed")))
+    : filteredTaskList;
 
-  renderList(filteredTaskList);
+  renderList(filteredByStatus, status);
+
   localStorage.setItem("tasks", JSON.stringify(taskList.list));
 }
 
@@ -133,5 +220,4 @@ function loadTasksFromLocalStorage(): void {
   }
 }
 
-// Call this function on page load
 loadTasksFromLocalStorage();
