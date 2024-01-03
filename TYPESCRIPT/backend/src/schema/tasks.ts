8323f25ee1f2944dc434fa4c1bf254aeab881c7a
sadishaaaa <sthasadisha2019@gmail.com>
2024-01-03 17:19:31 +0545

    middleware added

--- TYPESCRIPT/backend/src/schema/tasks.ts
+++ TYPESCRIPT/backend/src/schema/tasks.ts
@@ -1,6 +1,8 @@
 import Joi from "joi";
 export const TaskSchema = Joi.object({
   task: Joi.string().required().max(255).messages({
-    "string.base": "Task is required",
+    "string.required": "Task is required",
+    "string.empty": "Task cannot be empty",
+    "string.max": `Task must not exceed 255 characters`
   }),
 });
--- TYPESCRIPT/backend/src/schema/users.ts
+++ TYPESCRIPT/backend/src/schema/users.ts
@@ -2,16 +2,30 @@ import Joi from "joi";
 export const UserSchema = Joi.object({
   username: Joi.string().required().max(50).min(5).messages({
     "string.base": "username is required",
+    "string.empty": "Task cannot be empty",
+    "string.required": "User Name is required",
+    "string.max": "User Name should not exeed  50 charcter ",
+    "string.min": "User Name should  have atleast 5 charcter ",
   }),
   email: Joi.string().required().max(50).min(10).email().messages({
     "string.email": "Email must be a valid format",
+    "string.empty": "Task cannot be empty",
+    "string.required": "Email is required",
+    "string.min": "Email should contain at least 10 characters",
+    "string.max": "Email should not exceed 50 characters",
   }),
   Address: Joi.string().required().max(50).messages({
-    "string.base": "Address should be in valid fromat",
+    "string.base": "Address should be in valid format",
+    "string.empty": "Task cannot be empty",
+    "string.required": "Address is required",
+    "string.max": "Address should not exceed 50 characters",
   }),
 
   PhoneNumber: Joi.string().required().min(10).messages({
     "string.base": "10 is the minimum requirement",
+    "string.required": "Phone Number is required",
+    "string.empty": "Task cannot be empty",
+    "string.min": "PhoneNumber should be at least 10",
   }),
   password: Joi.string()
     .required()
@@ -19,7 +33,9 @@ export const UserSchema = Joi.object({
     .max(10)
     .regex(/^[a-zA-Z0-9][^a-zA-Z0-9][a-zA-Z0-9]*$/)
     .messages({
-      "string.base":
-        "Password should be minimum of 5 character and max of 10 character",
+      "string.required": "Password is required",
+      "string.min": "Password should have minimum 5 Character",
+      "string.max": "Password should not exceed 10 Characters",
+      "string.empty": "Task cannot be empty",
     }),
 });
